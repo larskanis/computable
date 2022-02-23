b741742c1cab51febda5b7f1c260a07446ccfe02
Lars Kanis <kanis@comcard.de>
2022-02-23 13:40:35 +0100

    CI: Avoid timing dependent tests on platforms that are flaky

--- test/test_parallel.rb
+++ test/test_parallel.rb
@@ -40,35 +40,37 @@ class TestParallel < Minitest::Test
 #     @b.computable_display_dot
   end
 
-  def diff_time
-    st = Time.now
-    res = yield
-    [Time.now - st, res]
-  end
+  unless ENV["NO_TIMING_TESTS"]=="true"
+    def diff_time
+      st = Time.now
+      res = yield
+      [Time.now - st, res]
+    end
 
-  def test_full_parallel
-    Computable.computable_max_threads = nil
-    @b.i1, @b.i2 = 2, 3
-    dt, res = diff_time{ @b.o }
-    assert_in_delta 0.8, dt, 0.05
-    assert_equal 20, res
+    def test_full_parallel
+      Computable.computable_max_threads = nil
+      @b.i1, @b.i2 = 2, 3
+      dt, res = diff_time{ @b.o }
+      assert_in_delta 0.8, dt, 0.05
+      assert_equal 20, res
 
-    @b.i1, @b.i2 = 4, 5
-    dt, res = diff_time{ @b.o }
-    assert_in_delta 0.2, dt, 0.05
-    assert_equal 36, res
-  end
+      @b.i1, @b.i2 = 4, 5
+      dt, res = diff_time{ @b.o }
+      assert_in_delta 0.2, dt, 0.05
+      assert_equal 36, res
+    end
 
-  def test_2_threads
-    Computable.computable_max_threads = 2
-    @b.i1, @b.i2 = 2, 3
-    dt, res = diff_time{ @b.o }
-    assert_in_delta 0.8, dt, 0.05
-    assert_equal 20, res
+    def test_2_threads
+      Computable.computable_max_threads = 2
+      @b.i1, @b.i2 = 2, 3
+      dt, res = diff_time{ @b.o }
+      assert_in_delta 0.8, dt, 0.05
+      assert_equal 20, res
 
-    @b.i1, @b.i2 = 4, 5
-    dt, res = diff_time{ @b.o }
-    assert_in_delta 0.4, dt, 0.05
-    assert_equal 36, res
+      @b.i1, @b.i2 = 4, 5
+      dt, res = diff_time{ @b.o }
+      assert_in_delta 0.4, dt, 0.05
+      assert_equal 36, res
+    end
   end
 end
